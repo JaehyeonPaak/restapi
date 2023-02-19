faa8cc486eac96baa21925097e39f38d0a78ea62
JaehyeonPaak <wogus392@gmail.com>
2023-02-19 15:42:23 +0900

    feat: implementing userrepository class

---
+++ src/main/java/com/restapi/app/ws/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.restapi.app.ws.repository;
+
+import com.restapi.app.ws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
--- src/main/java/com/restapi/app/ws/ui/service/impl/UserServiceImpl.java
+++ src/main/java/com/restapi/app/ws/ui/service/impl/UserServiceImpl.java
@@ -1,14 +1,33 @@
 package com.restapi.app.ws.ui.service.impl;
 
+import com.restapi.app.ws.io.entity.UserEntity;
+import com.restapi.app.ws.repository.UserRepository;
 import com.restapi.app.ws.shared.dto.UserDto;
 import com.restapi.app.ws.ui.service.UserService;
+import org.apache.catalina.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setUserId("testUserId");
+        userEntity.setEncryptedPassword("test");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
