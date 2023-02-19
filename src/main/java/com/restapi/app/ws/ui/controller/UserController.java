5296e6a65ffcf5f919fbd86e310a949ca7c9792f
JaehyeonPaak <wogus392@gmail.com>
2023-02-19 13:20:01 +0900

    feat: making use of userrest and userdto in restcontroller

--- src/main/java/com/restapi/app/ws/ui/controller/UserController.java
+++ src/main/java/com/restapi/app/ws/ui/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.restapi.app.ws.ui.controller;
 
+import com.restapi.app.ws.shared.dto.UserDto;
 import com.restapi.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.restapi.app.ws.ui.model.response.UserRest;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +17,15 @@ public class UserController {
 
     @PostMapping
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
-        return null;
+        UserRest returnValue = new UserRest();
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+
+        UserDto createdUser = new userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+
+        return returnValue;
     }
 
     @PutMapping
