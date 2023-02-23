6af2e092c9f7a8822ba35d2d3396552fcf198404
wogus392 <wogus392@nexol.co.kr>
2023-02-23 12:24:31 +0900

    feat: generate user secure  public id

--- src/main/java/com/restapi/app/ws/ui/service/impl/UserServiceImpl.java
+++ src/main/java/com/restapi/app/ws/ui/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.restapi.app.ws.ui.service.impl;
 
 import com.restapi.app.ws.io.entity.UserEntity;
 import com.restapi.app.ws.repository.UserRepository;
+import com.restapi.app.ws.shared.Utils;
 import com.restapi.app.ws.shared.dto.UserDto;
 import com.restapi.app.ws.ui.service.UserService;
 import org.apache.catalina.User;
@@ -15,6 +16,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
 
@@ -26,7 +30,11 @@ public class UserServiceImpl implements UserService {
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
-        userEntity.setUserId("testUserId");
+        // This secure public user ID will be used to identify and find records in database
+        // e.g. mobile application can use this user ID and request to get the details of this particular user
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
+
         userEntity.setEncryptedPassword("test");
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
