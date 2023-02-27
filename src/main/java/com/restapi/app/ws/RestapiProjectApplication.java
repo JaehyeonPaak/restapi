2b5336edc30fb5e5b2fd8f295d5759eeccfd9721
wogus392 <wogus392@nexol.co.kr>
2023-02-27 17:59:46 +0900

    feat: encrypt user password

--- src/main/java/com/restapi/app/ws/RestapiProjectApplication.java
+++ src/main/java/com/restapi/app/ws/RestapiProjectApplication.java
@@ -2,12 +2,19 @@ package com.restapi.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class RestapiProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestapiProjectApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/restapi/app/ws/ui/service/impl/UserServiceImpl.java
+++ src/main/java/com/restapi/app/ws/ui/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.restapi.app.ws.ui.service.UserService;
 import org.apache.catalina.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDto createUser(UserDto user) {
@@ -35,7 +39,7 @@ public class UserServiceImpl implements UserService {
         String publicUserId = utils.generateUserId(30);
         userEntity.setUserId(publicUserId);
 
-        userEntity.setEncryptedPassword("test");
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
